f74c1d8033e62b21e6e550979f6d91e2d7f1f099
hyuoh <hyuoh421@gmail.com>
2022-11-28 19:07:59 +0900

    221128

--- Korea Univ/2022-2R/역공학/task1.c
+++ Korea Univ/2022-2R/역공학/task1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int sub_140001300(int *a1, int a2, int *a3, int a4, int a5, int a6)
+int sub_140001300(int *a1, unsigned int a2, int *a3, int a4, char a5, char a6)
 {
     int i;
     if (a2 != a4)
@@ -15,14 +15,14 @@ int sub_140001300(int *a1, int a2, int *a3, int a4, int a5, int a6)
 
 int main()
 {
-    int Str2[20] = 0;
-    int aJvUlHBvxrEc1d[20] = { 0x4a, 0x56, 0x28, 0x75, 0x6c, 0x7d, 0x68, 0x7d, 0x62, 0x76, 0x58, 0x72, 0x28, 0x45, 0x43, 0x31, 0x44, 0x5b, 0x40 };
+    int Str2[20] = {0};
+    int aJvUlHBvxrEc1d[20] = {0x4a, 0x56, 0x28, 0x75, 0x6c, 0x7d, 0x68, 0x7d, 0x62, 0x76, 0x58, 0x72, 0x28, 0x45, 0x43, 0x31, 0x44, 0x5b, 0x40};
 
-    sub_140001300(Str2[20], 20, aJvUlHBvxrEc1d[20], 20, 1, 2);
+    sub_140001300(Str2, 20, aJvUlHBvxrEc1d, 20, 1, 2);
 
     for (int i = 0; i < 20; i++)
     {
-        printf("Str[%d] is %d\n", i, Str2[i]);
+        printf("Str2[%d] is %d\n", i, Str2[i]);
     }
 
     return 0;
